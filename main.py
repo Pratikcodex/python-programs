b457bf4f72a4f9bd0823da055851383616f5b945
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-09 15:29:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import turtle
+# square
+pratik_turtle = turtle.Turtle()
+pratik_turtle.speed(7)
+
+
+def square():#we define a function here 
+
+    pratik_turtle.forward(100)
+    pratik_turtle.right(90)
+    pratik_turtle.forward(100)
+    pratik_turtle.right(90)
+    pratik_turtle.forward(100)
+    pratik_turtle.right(90)
+    pratik_turtle.forward(100)
+
+
+square()#now above large code is stored in it now 
+print()
+
+import calendar
+#used to output days in week
+print(calendar.weekheader(3))
+print() #using another one to find mon as 0
+print(calendar.firstweekday())
+print() 
+# using this to find out the month via year and date 
+print(calendar.month(2022,9))
+print()#in matrix formation of above
+print(calendar.monthcalendar(2022,9))
+print()#printing the whole fucking year 2022
+print(calendar.calendar(2022))
+print()#printing the no of the week day
+day_of_the_week =calendar.weekday(2022,8,9)
+print(day_of_the_week)
+print()#finding whearher a year is a leap year or not
+is_leap = calendar.isleap(2020)
+print (is_leap)
+print()
+how_many_leap_days = calendar.leapdays(2000,2023)
+print(how_many_leap_days)
+print()
+
+elephant_weight =200
+ant_weight=2
+
+if elephant_weight > ant_weight:
+  square()
+else:
+  pratik_turtle.forward(100)
+  print('holla')
+  #while loop done
+  #for loop indentation problem
+  
+  
+  
+
+
+ 
+
+
+
