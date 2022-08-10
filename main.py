1cfcc4d6b82986c77cb59093e6e8febe9003b8ba
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-10 05:20:15 +0000

    variables and for loop with lists

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
+#using variables with for loops
 digits = [0,1,2,3,4,5,6,7,8,9]
-names = "happy coder"
 
-print(digits[1])
-
-print()
-
-print(digits[-1])
+for i in range (len(digits)):
+  print(digits[0:i])
 
 print()
-
-#parts of it 
-
-print(digits[:3])
-
-print()
-
-print(names[:6])
-print(names[5:])
-
-print()
-
-#stride
-print(digits[0:10:2] )
-#stride means jumping over
-print(digits[0:10:3])
-print()
+win_size = 2 #for bigger things
+for i in range (len(digits)-win_size-1):#we minus one less than the window size
+  print(digits[i:i+win_size])
