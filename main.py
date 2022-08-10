99bef48942e9083a2dee68d76c6232852f14703a
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-10 04:30:02 +0000

    reverse the list

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-#learning lists
-
-l = [1,2,3]
-print (l[1])
-print()
-
-#complex lists
-list2 =["string" ,'singel', True,4.2,[1,2,3]]
-print(list2[0])
-print(list2[1])
-print(list2[2])
-print(list2[3])
-print(list2[4])
-print()
-
-#append in lists (puting something at the end of the list
-
-names =["john","bro","lol"]
+names =["john", "bro" , "raj"]
 print(names)
-print()
-names.append("gary")
+names.reverse()
 print(names)
-print()
-names.insert(0,"end")#any point 1,2,3
-print(names)
-print()
-#we can use remove 
