dc6c52fde889f25fa0f992dee4849eb22556e8ee
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-10 04:42:32 +0000

    sorting and for loop in lists

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-names =["john", "bro" , "raj"]
-print(names)
-names.reverse()
-print(names)
+numbers =[6,3,9,1]
+
+print(numbers)
+
+print()
+
+numbers.sort()
+
+print(numbers)
+print()
+
+for number in numbers:
+  
+  print(number)
+
+#to print the numbers as many times as there are n inside the list use
+#print(numbers) instead of number
