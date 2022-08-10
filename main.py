c86d0aed973528e3fd6928ab8788fc5aae137e0f
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-10 05:03:31 +0000

    slicing and strides {indexing}

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-numbers =[6,3,9,1]
+digits = [0,1,2,3,4,5,6,7,8,9]
+names = "happy coder"
 
-print(numbers)
+print(digits[1])
 
 print()
 
-numbers.sort()
+print(digits[-1])
 
-print(numbers)
 print()
 
-for number in numbers:
-  
-  print(number)
+#parts of it 
 
-#to print the numbers as many times as there are n inside the list use
-#print(numbers) instead of number
+print(digits[:3])
+
+print()
+
+print(names[:6])
+print(names[5:])
+
+print()
+
+#stride
+print(digits[0:10:2] )
+#stride means jumping over
+print(digits[0:10:3])
+print()
