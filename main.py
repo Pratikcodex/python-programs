45c13575b4ad5446f84da9b447158d59fdeb703e
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-11 04:34:59 +0000

    split and join

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-#using variables with for loops
-digits = [0,1,2,3,4,5,6,7,8,9]
+#learning split
+problems = 'broke, pale, short, nerdy'
+print(problems)
+print()
 
-for i in range (len(digits)):
-  print(digits[0:i])
+l=problems.split(", ")
+print(l)
 
 print()
-win_size = 2 #for bigger things
-for i in range (len(digits)-win_size-1):#we minus one less than the window size
-  print(digits[i:i+win_size])
+#l2 =problems.split("short")
+#print(l2)
+
+print()
+
+#learning join
+joined = ' and '.join(l)
+
+print(joined)
+print()
+
+csv = ','.join(l)
+print(csv)
