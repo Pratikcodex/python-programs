f33f0d2c69617d902f4ea5b8732e5d92e38dd73c
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-11 13:48:25 +0000

    adding into set

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-person1 = ("nancy_pants", 25 ,'pizza')
-print()
-person2 = ("joe", 35 ,'burger')
+s ={'blueberry', 'rasberry'}
 
-people =[person1 ,person2]
+s.add('strawberry')
 
-#name , age , favfood = person
-#print(name)
-#print(age)
-#print(favfood)
-
-for name, age, favfood in people:
-  print(name)
-  print(age)
-  print(favfood)
-  print()
-    
+print(s)
