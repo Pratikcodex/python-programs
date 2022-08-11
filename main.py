7c15a7b18a8dcda2edb95fb342f4d5039883b101
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-11 14:10:02 +0000

    union , intersection and difference of sets

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-list_of_numbers = [1,1,1,2,2,3,3,4,4,5,5,5,]
+library_1 ={'harry', 'game of thrones', 'potter'}
+
+library_2 = {'harry', 'hola'}
+
+all_books_in_town = library_1.union(library_2)
+
+same_books_in_town=library_1.intersection(library_2)
+
+print(all_books_in_town)#union
+
+print()
+
+print(same_books_in_town)#intersection
 
 
+print()
 
-number_duplicate_set=set(list_of_numbers)
+#difference
 
-number_duplicate_set=list(number_duplicate_set)
-
-list_of_numbers = number_duplicate_set
-
-print(number_duplicate_set)
-
+diff = library_1.difference(library_2)
+diff2 = library_2.difference(library_1)
+print(diff)
+print()
+print(diff2)
 
