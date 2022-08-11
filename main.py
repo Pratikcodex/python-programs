72f460255992507b2e0f4eff333109d56ba906b3
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-11 13:55:23 +0000

    duplicate clearance in set

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-s ={'blueberry', 'rasberry'}
+list_of_numbers = [1,1,1,2,2,3,3,4,4,5,5,5,]
 
-s.add('strawberry')
 
-print(s)
+
+number_duplicate_set=set(list_of_numbers)
+
+number_duplicate_set=list(number_duplicate_set)
+
+list_of_numbers = number_duplicate_set
+
+print(number_duplicate_set)
+
+
