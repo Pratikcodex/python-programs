3ce0bac288b3878c155e58ff4e33543730fbb7a7
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-12 15:57:24 +0000

    EMAIL SEARCHER (REGEX)

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
 import re
 
-text = "a Random string."
+text = "a Random string  pratikrajcpr@website.com  YOURNAME@company.net"
 
-pattern = re.compile("a Random string")
+pattern = re.compile("[a-zA-Z0-9,]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
 
-result = pattern.search(text)
+#result = pattern.search(text)#only search for first function
 
+result = pattern.findall(text)
 print(result)
 
-wolla = "A bug in the House"
-
-pattern = re.compile("[ABC]")
-
-result2 = pattern.search(wolla)
-
-print(result2)
-
-
-
-text2 = "Random546 string."
-
-pattern = re.compile("[a-zA-Z0-9]+")
-
-result3 = pattern.search(text2)
-
-print(result3)
 
 
